c353f9682fa5838c676b94c1cbd6106723c8eda1
Administrator <1746678613@qq.com>
2020-12-30 15:14:22 +0800

    bran1 --plus：Ar实现page和curd

--- pm-001/src/test/java/com/zhang/pm/Test01.java
+++ pm-001/src/test/java/com/zhang/pm/Test01.java
@@ -1,9 +1,13 @@
 package com.zhang.pm;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zhang.pm.beans.Employee;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
 
 /**
  * @author: create by zhl
@@ -26,6 +30,35 @@ public class Test01 {
         employee.setLastName("zhang");
         boolean insert = employee.insert();
         System.out.println(insert);
+
+    }
+
+    /**
+     * Ar实现curd
+     */
+    @Test
+    public void test002(){
+
+        Employee employee=new Employee();
+
+        List<Employee> employees = employee.selectList(new QueryWrapper<Employee>().like("last_name","1"));
+        for (Employee employee1 : employees) {
+
+            System.out.println(employee1);
+        }
+
+    }
+    /**
+     * Ar实现page
+     */
+    @Test
+    public void test003(){
+
+        Employee employee=new Employee();
+
+        Page<Employee> employeePage = employee.selectPage(new Page<Employee>(1,2), new QueryWrapper<Employee>().eq("last_name", "zhang"));
+        System.out.println(employeePage.getPages());
+        System.out.println(employeePage.getRecords());
 
     }
 }
