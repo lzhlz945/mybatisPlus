65f2d69e4897869b096addeae9bbef40292d8a84
Administrator <1746678613@qq.com>
2020-12-30 09:39:55 +0800

    bran1 --plus条件选择器：EntityWrapper

--- pm-001/src/test/java/com/zhang/pm/AppTest.java
+++ pm-001/src/test/java/com/zhang/pm/AppTest.java
@@ -2,6 +2,8 @@ package com.zhang.pm;
 
 import static org.junit.Assert.assertTrue;
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.plugins.Page;
 import com.zhang.pm.beans.Employee;
 import com.zhang.pm.mapper.EmployeeMapper;
 import org.junit.Test;
@@ -57,4 +59,40 @@ public class AppTest {
 
 
     }
+    /**
+     * 测试EntityWrapper1条件构造器
+     */
+    @Test
+    public void testEntityWrapper(){
+        List<Employee> employeeList = employeeMapper.selectPage(new Page<Employee>(1, 2), new EntityWrapper<Employee>()
+                .between("age", 18, 50).eq("last_name", "zhang")
+                .eq("gender", 1));
+        for (Employee employee : employeeList) {
+            System.out.println(employee);
+        }
+
+    }
+    /**
+     * 测试EntityWrapper2条件构造器
+     */
+    @Test
+    public void testEntityWrapper1(){
+        Employee employee=new Employee();
+        employee.setAge(18);
+        employee.setEmail("zs@qq.com");
+        employee.setGender(1);
+        employee.setLastName("zhang");
+        //employeeMapper.insert(employee);
+        //Integer id = employee.getId();
+        List<Integer> list=new ArrayList<>();
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        List<Employee> selectList = employeeMapper.selectList(new EntityWrapper<Employee>().eq("last_name", "zhang"));
+        for (Employee employee1 : selectList) {
+            System.out.println(employee1);
+        }
+
+    }
+
 }
